72d2eb4182eef0a9e959a444ee092d7a0bbc5e16
JCPB2000 <jcpaguay1@espe.edu.ec>
2025-06-05 11:08:46 -0500

    Se función js para calcular resultado

---
+++ public/js/calculadora.js
@@ -0,0 +1,20 @@
+let operacion = "";
+
+function seleccionarOperacion(op) {
+  operacion = op;
+
+  let resultado = document.getElementById("resultado_final");
+  resultado.textContent = "OPERACIÓN SELECCIONADA: " + op;
+
+  // Limpiar los campos para ingresar el segundo número
+  document.getElementById("txt_numero").disabled = true;
+}
+
+function calcularResultado() {
+  let numero1 = document.getElementById("txt_numero").value;
+  let numero2 = document.getElementById("txt_numero_2").value;
+
+  let resultado = document.getElementById("resultado_final");
+
+  
+}
